79dfd069d2f8c208d84edf3518ad2cdb18a524bb
hitarth.rohra <hitarth.rohra@gmail.com>
2024-01-06 04:11:42 +0530

    Resolving pychache dependencies

--- pages/contact.py
+++ pages/contact.py
@@ -19,7 +19,7 @@ def main():
     st.write("""
     **For any inquiries or feedback, please contact us at hitarth.rohra@gmail.com**
     """) 
-    st.markdown("### Acknowledgments:")
+    st.markdown("#### Acknowledgments:")
     st.write("We would like to thank the Streamlit community and the developers of the underlying libraries that made this app possible.")
 
 if __name__ == "__main__":
