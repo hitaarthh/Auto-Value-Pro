4ede8983116644da856aeb23dbb3c81462c53f7f
hitarth.rohra <hitarth.rohra@gmail.com>
2024-01-06 01:48:04 +0530

    removed about.py

--- pages/about_page.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-
-def about_page():
-    # Content for the About page
-    st.title("About Auto Value Pro")
-    st.write("This is the About page of Auto Value Pro.")
-    # Add more content as needed
