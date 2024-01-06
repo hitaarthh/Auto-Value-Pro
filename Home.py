52079574c59e49fa9097f92cf44233b627b0fe8c
hitarth.rohra <hitarth.rohra@gmail.com>
2024-01-07 01:44:10 +0530

    Added Linear Regression Model

--- Home.py
+++ Home.py
@@ -1,8 +1,23 @@
 import streamlit as st
 import time
+import pandas as pd
+import pickle
+import os
+
+#Importing datasets and the Machine learning models
+df = pd.read_csv("dataset/Cleaned_Car_data.csv")
+model_path = os.path.join('model', 'LinearRegressionModel.pkl')
+
+#Loading the pickle file of the model.
+lrModel = pickle.load(open(model_path, 'rb'))
+
+#<!-The Machine Learning importing part ends here!>
 
 st.set_page_config(page_title=f"Auto Value Pro", page_icon="🚗", layout="wide")
-
+favicon_html = """
+    <link rel="shortcut icon" href="speed.png" type="image/x-icon">
+"""
+st.markdown(favicon_html, unsafe_allow_html=True)
 # Custom CSS for styling
 custom_css = """
     <style>
@@ -16,11 +31,8 @@ custom_css = """
     }
     </style>
 """
-
 # Apply custom CSS
 st.markdown(custom_css, unsafe_allow_html=True)
-
-
 # Sidebar
 st.sidebar.markdown("You are on the Home Page")
 
@@ -29,8 +41,8 @@ st.markdown("<h1 class='title-text'>Auto Value Pro : A Vehicle Valuation Wizard<
 st.markdown("<h5 style='text-align: center;'>This app predicts the price of a car you want to sell or buy. Try filling the details below:</h5>", unsafe_allow_html=True)
 
 # Form to accept data
-company_options = ["Company A", "Company B", "Company C"]  # Add your actual list of car companies
-model_options = ["Model X", "Model Y", "Model Z"]  # Add your actual list of car models
+ # Add your actual list of car companies
+company_options = sorted(df['company'].unique().tolist())
 year_options = list(range(2000, 2021))[::-1]  # Reverse the list of years from 2000 to 2020
 year_options.append(1995)
 fuel_options = ["Petrol", "Diesel", "LPG"]
@@ -46,9 +58,24 @@ algorithm_options = [
 # User input forms
 col1, col2 = st.columns(2)
 with col1:
+    # Selectbox for choosing the company
     selected_company = st.selectbox(f"##### Select the Company:", company_options)
+
+# Filter the unique car names based on the selected company
+carname = df[df['company'] == selected_company]['name'].unique().tolist()
+
+# Initialize model_options with a default value
+model_options = ['default value']
+
+if carname:
+    # Extract models for the selected company based on the length of the company name
+    company_length = len(selected_company)
+    model_options = [f"{selected_company} {car[company_length+1:]}" for car in carname if car.startswith(selected_company + ' ')]
+    model_options.sort()
+
 with col2:
     selected_model = st.selectbox("##### Select the Model:", model_options)
+
 
 col3, col4 = st.columns(2)
 with col3:
@@ -74,27 +101,66 @@ with col9:
 with col10:
     modelReport = st.button("Model Report", help="Click to model report", key="model_report", use_container_width=True)
 
+
+testingData = pd.DataFrame({
+    'name':selected_model,
+    'company':selected_company,
+    'year': selected_year,
+    'kms_driven':kilometers_driven,
+    'fuel_type':selected_fuel
+},index=[0])
+
+
 max_thresholdDriven = 200000
 base_price_for_high_mileage = 100000
 
-def perform_actions(selected_company, selected_model, selected_year, kilometers_driven, transmission_type, num_previous_owners, selected_algorithm, max_thresholdDriven):
+def perform_actions(selected_company, selected_model, selected_year, kilometers_driven, transmission_type, num_previous_owners, selected_algorithm, max_thresholdDriven,prediction):
     if kilometers_driven < max_thresholdDriven:
          st.success(f"###### Predicting car price for {selected_company} {selected_model} ({selected_year}) with {kilometers_driven} kms driven, {transmission_type} transmission, and {num_previous_owners} previous owner(s) using {selected_algorithm} algorithm.")
+         st.success(f"###### Predicted Price: {round(prediction[0], 2)}")
     else: 
         st.success(f"Attention: Your car has been driven more than {max_thresholdDriven} kilometers, indicating high mileage.")
         st.success(f"Considering this, the estimated selling price has been adjusted to a base price of ${base_price_for_high_mileage}.")
 
-def modelReportFunc(selected_company, selected_model, selected_year, kilometers_driven, transmission_type, num_previous_owners, selected_algorithm, max_thresholdDriven):
+def modelReportFunc(selected_company, selected_model, selected_year, kilometers_driven, transmission_type, num_previous_owners, selected_algorithm, max_thresholdDriven,prediction):
     with st.spinner("Generating model report..."):
         time.sleep(3)  # Simulate a delay for calculations
-    perform_actions(selected_company, selected_model, selected_year, kilometers_driven, transmission_type, num_previous_owners, selected_algorithm, max_thresholdDriven)
+    perform_actions(selected_company, selected_model, selected_year, kilometers_driven, transmission_type, num_previous_owners, selected_algorithm, max_thresholdDriven,prediction)
     st.markdown(f"##### Model Report:")
     st.markdown(f"- Here are the attributes associated with the chosen algorithm, {selected_algorithm}, revealing the model's performance indicators.")
+
+def model_selection():
+    y_predict=[]
+    if selected_algorithm == "Linear Regression":
+        y_predict = lrModel.predict(testingData)
+    elif selected_algorithm == "Random Forest Regressor":
+        y_predict = lrModel.predict(testingData)
+    elif selected_algorithm ==  "Decision Trees Regressor":
+        y_predict = lrModel.predict(testingData)
+    elif selected_algorithm == "Gradient Boosting":
+        y_predict = lrModel.predict(testingData)
+    elif selected_algorithm == "Support Vector Machines (SVM)":
+        y_predict = lrModel.predict(testingData)
+    elif selected_algorithm == "K-Nearest Neighbors (KNN)":
+        y_predict = lrModel.predict(testingData)
+    else:
+        y_predict=["0.00"]
+    return y_predict
 
 # Display results if the button is pressed
 if predict_button:
     with st.spinner("Calculating the price..."):
         time.sleep(3)  # Simulate a delay for calculations
-    perform_actions(selected_company, selected_model, selected_year, kilometers_driven, transmission_type, num_previous_owners, selected_algorithm, max_thresholdDriven)
+    prediction=[]
+    prediction = model_selection()
+    perform_actions(selected_company, selected_model, selected_year, kilometers_driven, transmission_type, num_previous_owners, selected_algorithm, max_thresholdDriven,prediction)
+
 if modelReport:
-    modelReportFunc(selected_company, selected_model, selected_year, kilometers_driven, transmission_type, num_previous_owners, selected_algorithm, max_thresholdDriven)
+        prediction = model_selection()
+        modelReportFunc(selected_company, selected_model, selected_year, kilometers_driven, transmission_type, num_previous_owners, selected_algorithm, max_thresholdDriven,prediction)
+
+
+
+
+
+
