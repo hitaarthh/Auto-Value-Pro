e3cad106e24d5a2cd0584c75dcb4c4887ad7b57a
hitarth.rohra <hitarth.rohra@gmail.com>
2024-01-06 12:03:47 +0530

    Edge cases code encapsulated

--- home.py
+++ home.py
@@ -91,17 +91,32 @@ with col9:
 with col10:
     modelReport = st.button("Model Report", help="Click to model report", key="model_report")
 
+max_thresholdDriven = 200000
+base_price_for_high_mileage = 100000
 
-# Display results if the button is pressed
-if predict_button:
-    st.success(f"Predicting car price for {selected_company} {selected_model} ({selected_year}) with {kilometers_driven} km driven, {transmission_type} transmission, and {num_previous_owners} previous owner(s) using {selected_algorithm} algorithm.")
+def perform_actions(selected_company, selected_model, selected_year, kilometers_driven, transmission_type, num_previous_owners, selected_algorithm, max_thresholdDriven):
+    if kilometers_driven < max_thresholdDriven:
+         st.success(f"Predicting car price for {selected_company} {selected_model} ({selected_year}) with {kilometers_driven} kms driven, {transmission_type} transmission, and {num_previous_owners} previous owner(s) using {selected_algorithm} algorithm.")
+    else: 
+        st.success(f"Attention: Your car has been driven more than {max_thresholdDriven} kilometers, indicating high mileage.")
+        st.success(f"Considering this, the estimated selling price has been adjusted to a base price of ${base_price_for_high_mileage}.")
+        # st.success(f"Original Predicted Price: ${predicted_price}")
+        # st.success(f"Adjusted Selling Price: ${adjusted_price}")
     # Add your machine learning prediction logic here and display the result
     # For example: predicted_price = predict_price(selected_company, selected_model, selected_year, selected_fuel, kilometers_driven, selected_algorithm, transmission_type, num_previous_owners)
     # st.write(f"Predicted Price: ${predicted_price}")
-if modelReport:
-    st.success(f"Predicting car price for {selected_company} {selected_model} ({selected_year}) with {kilometers_driven} km driven, {transmission_type} transmission, and {num_previous_owners} previous owner(s) using {selected_algorithm} algorithm.")
+
+def modelReportFunc(selected_company, selected_model, selected_year, kilometers_driven, transmission_type, num_previous_owners, selected_algorithm, max_thresholdDriven):
+    perform_actions(selected_company, selected_model, selected_year, kilometers_driven, transmission_type, num_previous_owners, selected_algorithm, max_thresholdDriven)
     st.markdown(f"Model Report:")
     st.markdown(f"- Here are the attributes associated with the chosen algorithm, {selected_algorithm}, revealing the model's performance indicators.")
+
+# Display results if the button is pressed
+if predict_button:
+    perform_actions(selected_company, selected_model, selected_year, kilometers_driven, transmission_type, num_previous_owners, selected_algorithm, max_thresholdDriven)
+if modelReport:
+    modelReportFunc(selected_company, selected_model, selected_year, kilometers_driven, transmission_type, num_previous_owners, selected_algorithm, max_thresholdDriven)
+    
 # if page == "Home":
 #     home.show()
 # elif page == "About":
