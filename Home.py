9e8d57e52210f2ff5edfe7d42a281a0b759b600a
hitarth.rohra <hitarth.rohra@gmail.com>
2024-01-08 14:54:49 +0530

    CSS styling

--- Home.py
+++ Home.py
@@ -10,13 +10,14 @@ import matplotlib.pyplot as plt
 #Importing datasets and the Machine learning models
 df = pd.read_csv("dataset/Cleaned_Car_data.csv")
 
-#Loading the pickle file of the model.
+#Importing the pickle file of the model.
 lrmodel_path = os.path.join('model', 'LinearRegressionModel.pkl')
 dtmodel_path = os.path.join('model', 'DecisionTreeModel.pkl')
 rfmodel_path = os.path.join('model', 'RandomForestModel.pkl')
 svmmodel_path = os.path.join('model', 'SVMModel.pkl')
 gbmodel_path = os.path.join('model', 'GradientBoostModel.pkl')
 
+#Loading the dependencies from the Linear Regression model to the app.
 lrModel = pickle.load(open(lrmodel_path, 'rb'))
 lr_pipeline = lrModel['pipeline']
 lr_r2=lrModel['r2score']
@@ -26,6 +27,7 @@ lr_train_sizes = lrModel['train_sizes']
 lr_train_scores=lrModel['train_scores']
 lr_test_scores=lrModel['test_scores']
 
+#Loading the dependencies from the Decision Tree Regressor model to the app.
 dtModel = pickle.load(open(dtmodel_path, 'rb'))
 dt_pipeline = dtModel['pipeline']
 dt_r2=dtModel['r2score']
@@ -35,6 +37,7 @@ dt_train_sizes =dtModel['train_sizes']
 dt_train_scores=dtModel['train_scores']
 dt_test_scores=dtModel['test_scores']
 
+#Loading the dependencies from the Random Forest Regressor model to the app.
 rfModel = pickle.load(open(rfmodel_path, 'rb'))
 rf_pipeline = rfModel['pipeline']
 rf_r2=rfModel['r2score']
@@ -44,6 +47,7 @@ rf_train_sizes =rfModel['train_sizes']
 rf_train_scores=rfModel['train_scores']
 rf_test_scores=rfModel['test_scores']
 
+#Loading the dependencies from the Support Vector Machines (SVM) model to the app.
 svmModel = pickle.load(open(svmmodel_path, 'rb'))
 sv_pipeline = svmModel['pipeline']
 sv_r2=svmModel['r2score']
@@ -53,6 +57,7 @@ sv_train_sizes =svmModel['train_sizes']
 sv_train_scores=svmModel['train_scores']
 sv_test_scores=svmModel['test_scores']
 
+#Loading the dependencies from the Gradient Boosting model to the app.
 GradientBoostModel = pickle.load(open(gbmodel_path, 'rb'))
 gb_pipeline = GradientBoostModel['pipeline']
 gb_r2=GradientBoostModel['r2score']
@@ -62,9 +67,10 @@ gb_train_sizes =GradientBoostModel['train_sizes']
 gb_train_scores=GradientBoostModel['train_scores']
 gb_test_scores=GradientBoostModel['test_scores']
 
+#Function to display the model report.
+#gets imported onClick "Stats for nerd" button.
 def modelMetrics():
     st.write(f"#### Model Metrics:")
-
     if selected_algorithm == "Linear Regression":
         metrics_data = {
             'count':[1,2,3],
@@ -83,7 +89,6 @@ def modelMetrics():
         metrics_df.set_index('count', inplace=True)
         st.table(metrics_df)
         learningCurve(dt_train_sizes,dt_train_scores,dt_test_scores)
-
     elif selected_algorithm == "Random Forest Regressor":
         metrics_data = {
             'count':[1,2,3],
@@ -93,7 +98,6 @@ def modelMetrics():
         metrics_df.set_index('count', inplace=True)
         st.table(metrics_df)
         learningCurve(rf_train_sizes,rf_train_scores,rf_test_scores)
-
     elif selected_algorithm == "Gradient Boosting":
         metrics_data = {
             'count':[1,2,3],
@@ -103,7 +107,6 @@ def modelMetrics():
         metrics_df.set_index('count', inplace=True)
         st.table(metrics_df)
         learningCurve(gb_train_sizes,gb_train_scores,gb_test_scores)
-
     elif selected_algorithm == "Support Vector Machines (SVM)":
         metrics_data = {
             'count':[1,2,3],
@@ -113,10 +116,11 @@ def modelMetrics():
         metrics_df.set_index('count', inplace=True)
         st.table(metrics_df)
         learningCurve(sv_train_sizes,sv_train_scores,sv_test_scores)
-
     else:
         st.write(f"Default Model Report")
 
+#Function to display the learning curve of the model
+#gets imported within the modelMetrics()
 def learningCurve(train_sizes, train_scores, test_scores, figsize=(8, 4)):
     st.write(f"#### Learning Curve:")
     train_scores_mean = -np.mean(train_scores, axis=1)
@@ -131,9 +135,11 @@ def learningCurve(train_sizes, train_scores, test_scores, figsize=(8, 4)):
     plt.tight_layout()
     st.pyplot(fig)
 
-#<!-The Machine Learning importing part ends here!>
-
+#<!-price prediction and report display ends here!>
+    
+# Frontend of the Streamlit app starts here...
 st.set_page_config(page_title=f"Auto Value Pro", page_icon="🚗", layout="wide")
+
 # Custom CSS for styling
 custom_css = """
     <style>
@@ -147,19 +153,20 @@ custom_css = """
     }
     </style>
 """
-# Apply custom CSS
+
+# Applying custom CSS
 st.markdown(custom_css, unsafe_allow_html=True)
-# Sidebar
+
+# Sidebar Configuration
 st.sidebar.markdown("You are on the Home Page")
 
 # Title, subtitle, and description with center alignment
 st.markdown("<h1 class='title-text'>Auto Value Pro : A Vehicle Valuation Wizard</h1>", unsafe_allow_html=True)
 st.markdown("<h5 style='text-align: center;'>This app predicts the price of a car you want to sell or buy. Try filling the details below:</h5>", unsafe_allow_html=True)
 
-# Form to accept data
- # Add your actual list of car companies
-company_options = sorted(df['company'].unique().tolist())
-year_options = list(range(2000, 2021))[::-1]  # Reverse the list of years from 2000 to 2020
+#<!-Form to accept data-!>
+company_options = sorted(df['company'].unique().tolist()) #loading the unique values of the car company names
+year_options = list(range(2000, 2021))[::-1] #generating the various year options as per supervised dataset.
 year_options.append(1995)
 fuel_options = ["Petrol", "Diesel", "LPG"]
 algorithm_options = [
@@ -168,7 +175,7 @@ algorithm_options = [
     "Decision Trees Regressor",
     "Gradient Boosting",
     "Support Vector Machines (SVM)",
-]
+]#Defining different types of models on which predictions can be made.
 
 # User input forms
 col1, col2 = st.columns(2)
@@ -211,7 +218,7 @@ with col9:
 with col10:
     modelReport = st.button("Stats for nerds", help="Click to get model report", key="model_report", use_container_width=True)
 
-
+#Creating the testing data for model prediciton
 testingData = pd.DataFrame({
     'name':selected_model,
     'company':selected_company,
@@ -222,13 +229,13 @@ testingData = pd.DataFrame({
     'Transmission Type':transmission_type,
 },index=[0])
 
-
+#Maximum threshold value incaase the output because negative.
 max_thresholdDriven = 200000
 base_price_for_high_mileage = 100000
 
 def perform_actions(selected_company, selected_model, selected_year, kilometers_driven, transmission_type, num_previous_owners, selected_algorithm, max_thresholdDriven,prediction):
     value=prediction[0]
-    if kilometers_driven < max_thresholdDriven:
+    if kilometers_driven < max_thresholdDriven or value<=0:
          st.success(f"###### Predicting car price for {selected_company} {selected_model} ({selected_year}) with {kilometers_driven} kms driven, {transmission_type} transmission, and {num_previous_owners} previous owner(s) using {selected_algorithm} algorithm.")
          st.success(f"###### Predicted Price: ₹{round(value,2)}")
     else: 
@@ -238,7 +245,7 @@ def perform_actions(selected_company, selected_model, selected_year, kilometers_
 
 def modelReportFunc(selected_company, selected_model, selected_year, kilometers_driven, transmission_type, num_previous_owners, selected_algorithm, max_thresholdDriven,prediction):
     with st.spinner("Generating model report..."):
-        time.sleep(3)  # Simulate a delay for calculations
+        time.sleep(3)  
     perform_actions(selected_company, selected_model, selected_year, kilometers_driven, transmission_type, num_previous_owners, selected_algorithm, max_thresholdDriven,prediction)
     st.markdown(f"<h3 style='text-align: center; color: #FF504C;'>Model Report</h3>", unsafe_allow_html=True)
     st.markdown(f"##### Here are the attributes associated with the chosen algorithm, {selected_algorithm}, revealing the model's performance indicators.")
@@ -260,7 +267,7 @@ def model_selection():
     # y_predic=y_predict[0]
     return y_predict
 
-# Display results if the button is pressed
+#onclick "Predict Car Price" button
 if predict_button:
     with st.spinner("Calculating the price..."):
         time.sleep(3)  # Simulate a delay for calculations
@@ -268,12 +275,8 @@ if predict_button:
     prediction = model_selection()
     perform_actions(selected_company, selected_model, selected_year, kilometers_driven, transmission_type, num_previous_owners, selected_algorithm, max_thresholdDriven,prediction)
 
+#onClick "Stats for nerds"
 if modelReport:
         prediction = model_selection()
         modelReportFunc(selected_company, selected_model, selected_year, kilometers_driven, transmission_type, num_previous_owners, selected_algorithm, max_thresholdDriven,prediction)
         modelMetrics()
-
-
-
-
-
