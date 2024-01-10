b5aa99508378e3eccf59751545f1422ac9b1686a
hitarth.rohra <hitarth.rohra@gmail.com>
2024-01-11 00:48:24 +0530

    default

--- pages/About Us.py
+++ pages/About Us.py
@@ -25,9 +25,9 @@ def main():
     st.write("""
     Auto Value Pro, an advanced web app, predicts car prices using sophisticated machine learning. Users input details like make, model, and year, initiating dynamic analysis with models like Linear Regression as default.
 
-    ###### Ensemble models, including Decision Tree, Random Forest, Gradient Boosting, and Support Vector Machine, enhance accuracy. The app selects the best model for each scenario, ensuring precision tailored to each car.
+    Ensemble models, including Decision Tree, Random Forest, Gradient Boosting, and Support Vector Machine, enhance accuracy. The app selects the best model for each scenario, ensuring precision tailored to each car.
 
-    ###### While providing valuable insights, users are encouraged to see predictions as references. Real-world variations influenced by factors underscore the app's role as a guiding tool, aiding informed decisions in the dynamic automotive market.
+    While providing valuable insights, users are encouraged to see predictions as references. Real-world variations influenced by factors underscore the app's role as a guiding tool, aiding informed decisions in the dynamic automotive market.
               
     #### Technologies Used:
     - _Streamlit_
